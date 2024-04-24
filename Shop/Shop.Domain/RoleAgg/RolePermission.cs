fb89207070afafef4114d3ae893363e0980725db
mhmd.eb <mohammad.ebrahimi751120@gmail.com>
2024-04-24 16:01:18 +0330

    Role Aggregate & SellerAggregate Session 07

---
+++ Shop/Shop.Domain/RoleAgg/RolePermission.cs
@@ -0,0 +1,10 @@
+﻿using Common.Domain;
+using Shop.Domain.RoleAgg.Enums;
+
+namespace Shop.Domain.RoleAgg;
+
+public class RolePermission:BaseEntity
+{
+	public long RoleId { get;internal set; }
+	public Permission Permission { get; private set; }
+}
